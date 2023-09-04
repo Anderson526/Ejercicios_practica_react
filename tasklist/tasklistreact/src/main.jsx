4fb79ef84886edb364b36ca43cd06273663d6188
AndersonC <1megaword1@gmail.com>
2023-09-04 15:16:36 -0500

    css task list

--- tasklist/tasklistreact/src/main.jsx
+++ tasklist/tasklistreact/src/main.jsx
@@ -3,6 +3,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import TaskApp from './App.jsx'
+import './app.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
